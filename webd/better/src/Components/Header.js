d345a5a9e1ccd2a36b2c5005133c2b09bafda484
KrishikaAgrawal <agrawalkrishika592@gmail.com>
2024-08-05 23:59:13 +0530

    commit

---
+++ webd/better/src/Components/Header.js
@@ -0,0 +1,61 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <Navbar
+      expand="lg"
+      className=" bg-gray-200 px-3 py-2 sm:mb-1 sm:ml-1 sm:mr-1 rounded-t-lg sm:rounded-none"
+    >
+      <Container>
+        <Navbar.Brand>
+          {/* Logo container */}
+          <Link to="/Home/Home" className="text-2xl font-extrabold">
+            Better
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Navigation buttons */}
+          <Nav className="ml-auto gap-3 flex ">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "nav-link font-bold underline underline-offset-1"
+                  : "nav-link hover:underline hover:underline-offset-1"
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/About"
+              className={({ isActive }) =>
+                isActive
+                  ? "nav-link font-bold underline underline-offset-1"
+                  : "nav-link hover:underline hover:underline-offset-1"
+              }
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/MortgageCalculator"
+              className={({ isActive }) =>
+                isActive
+                  ? "nav-link font-bold underline underline-offset-1"
+                  : "nav-link hover:underline hover:underline-offset-1"
+              }
+            >
+              Mortgage Calculator
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
---
+++ webd/better/src/index.js
@@ -0,0 +1,55 @@
+/* import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
+import MortgageCalculator from "./Components/MortgageCalculator/MortgageCalculator";
+// import Error from "./Components/Error/Error";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "About",
+        element: <About />,
+      },
+      {
+        path: "MortgageCalculator",
+        element: <MortgageCalculator />,
+      },
+
+      // {
+      //   path: "*",
+      //   element: <Error />,
+      // },
+    ],
+  },
+]);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
