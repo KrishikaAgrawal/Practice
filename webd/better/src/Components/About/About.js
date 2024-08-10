3fe3b5da2ae6de3d9597ee71f591a0e441e52eed
KrishikaAgrawal <agrawalkrishika592@gmail.com>
2024-08-10 22:17:32 +0530

    commit

--- webd/better/src/Components/About/About.js
+++ webd/better/src/Components/About/About.js
@@ -13,12 +13,12 @@ const About = () => {
         </p>
       </div>
 
-      <div>
-        <div>
+      <div className="flex justify-between px-20 py-20">
+        <div className="w-1/2 px-16">
           <h1 className="text-3xl py-2 font-semibold text-[#292B29]">
             The status quo is broken
           </h1>
-          <p className="text-[#60625A] py-6 text-base">
+          <p className="text-[#60625A] py-6  text-base">
             The traditional processes around homeownership are opaque and
             stressful. Fees aren’t transparent and some are simply outrageous in
             size. Traditional mortgage lending is rife with unnecessary fees and
@@ -29,9 +29,33 @@ const About = () => {
             Read Vishal's Story
           </div>
         </div>
-        <div></div>
+        <div className="w-1/2 h-80 px-16">
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/1KjYlLBM9j4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
-      <div></div>
+
+      <div className=" text-white bg-[#017848] py-20 px-40 my-20">
+        <h1 className=" text-5xl pb-10 font-semibold">
+          How we’re changing things
+        </h1>
+        <p className="">
+          Homeownership is a huge part of our economy. Housing overall is a $33
+          trillion business, and mortgages account for $15 trillion. Yet home
+          finance operates in the same way it has for decades — through opaque
+          systems and expensive intermediaries whose interests are misaligned
+          with consumers’. <br /> <br /> That’s why Better.com is redefining the
+          homeownership process from the ground up. We’re using technology to
+          make it faster and more efficient, and humans to help make it friendly
+          and enjoyable.
+        </p>
+      </div>
     </>
   );
 };
