7ebe4e69aab487637614d0c0c991128e3502e8ec
Krishika Agrawal <137705225+KrishikaAgrawal@users.noreply.github.com>
2024-08-12 06:06:57 +0530

    Update About.js

--- webd/better/src/Components/About/About.js
+++ webd/better/src/Components/About/About.js
@@ -3,6 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <>
+<-- Mission --/>
       <div className="flex flex-col items-center py-36">
         <h1 className="text-[#017848] text-3xl py-10 font-semibold text-center">
           Our mission
