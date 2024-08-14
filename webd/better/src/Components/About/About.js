dcf40747abbcb978daa1356d180b74ed71a6165b
KrishikaAgrawal <agrawalkrishika592@gmail.com>
2024-08-15 00:45:36 +0530

    about.js complete

--- webd/better/src/Components/About/About.js
+++ webd/better/src/Components/About/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <>
-{/* Mission */}
+      {/* Mission */}
       <div className="flex flex-col items-center py-36">
         <h1 className="text-[#017848] text-3xl py-10 font-semibold text-center">
           Our mission
@@ -14,7 +14,7 @@ const About = () => {
         </p>
       </div>
 
-{/* The status quo is broken */}
+      {/* The status quo is broken */}
       <div className="flex justify-between px-20 py-20">
         <div className="w-1/2 px-16">
           <h1 className="text-3xl py-2 font-semibold text-[#292B29]">
@@ -58,87 +58,160 @@ const About = () => {
           and enjoyable.
         </p>
       </div>
-      <div>
-        <div className="relative">
-          {/* Vertical line */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 w-1 h-full bg-gray-300"></div>
 
-          {/* Timeline Items */}
-          <div className="space-y-8">
-            {/* 2014 */}
-            <div className=" flex  justify-center w-full">
-              <div className=" text-center w-1/12 p-2 bg-green-500 text-white rounded-full">
-                2014
+      <div className="relative">
+        {/* Vertical line */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 w-1 h-full bg-gray-300"></div>
+
+        {/* Timeline Items */}
+        <div className="space-y-8">
+          {/* 2014 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2014
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pr-24">
+            <div className=" text-left w-1/3 p-8 mr-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              After Vishal Garg’s first attempt to purchase his own dream home,
+              he quickly realized that the homebuying process is unnecessarily
+              broken. This inspired him to found a technology-first company led
+              by engineers and data experts with the mission of digitizing and
+              automating home finance to make it cheaper, easier, and faster for
+              all.
+            </div>
+          </div>
+
+          {/* 2015 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2015
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pl-24">
+            <div className=" text-left w-1/3 p-8 ml-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage funds its first mortgage loan entirely online
+              (without a single phone call!).
+            </div>
+          </div>
+
+          {/* 2016 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2016
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pr-24">
+            <div className=" text-left w-1/3 p-8 mr-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage becomes a Fannie Mae approved seller + servicer
+              and establishes relationships with top mortgage investors.
+            </div>
+          </div>
+
+          {/* 2017 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2017
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pl-24">
+            <div className=" text-left w-1/3 p-8 ml-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better expands into the real estate market with Better Real
+              Estate.
+            </div>
+          </div>
+
+          {/* 2018 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2018
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pr-24">
+            <div className=" text-left w-1/3 p-8 mr-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage partners with Ally Bank to build Ally powered by
+              Better.
+            </div>
+          </div>
+
+          {/* 2019 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2019
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pl-24">
+            <div className=" text-left w-1/3 p-8 ml-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage launches its pilot partnership with American
+              Express to deliver a seamless homebuying experience to AMEX
+              customers.
+            </div>
+          </div>
+
+          {/* 2021 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2021
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pr-24">
+            <div className=" text-left w-1/3 p-8 mr-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better acquires Trussle — The UK’s most innovative online mortgage
+              broker.
+            </div>
+          </div>
+
+          {/* 2022 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2022
+            </div>
+          </div>
+          <div className=" flex justify-center w-full pl-24">
+            <div className=" text-left w-1/3 p-8 ml-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage becomes the first fintech to fund $100B home loans
+              entirely online.
+            </div>
+          </div>
+
+          {/* 2023 */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              2023
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pr-24">
+            <div className=" text-left w-1/3 p-8 mr-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage launches One Day Mortgage¹: The first offering to
+              customers to go from application to full mortgage Commitment
+              Letter within 24 hours vs. typical industry process of 30+ days.
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pl-24">
+            <div className=" text-left w-1/3 p-8 ml-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage launches the fully digital 3-day HELOC².
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pr-24">
+            <div className=" text-left w-1/3 p-8 mr-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              Better Mortgage launches One Day Verified Approval Letter.
+            </div>
+          </div>
+
+          {/* Today */}
+          <div className=" flex  justify-center w-full">
+            <div className=" text-center w-1/12 p-2 bg-[#017848] font-semibold text-2xl text-white rounded-full">
+              Today
+            </div>
+          </div>
+          <div className=" flex  justify-center w-full pl-24">
+            <div className=" text-left w-1/3 p-8 ml-96 bg-[#DFE0DC] text-[#566162] rounded-lg">
+              You become part of the story by joining tens of thousands of happy
+              Better Mortgage borrowers.
+              <div className="p-3 bg-[#017848] font-semibold w-[120px] text-white rounded-lg text-center mt-4">
+                Get Started
               </div>
             </div>
-            <div className=" flex  justify-center w-full">
-              <div className=" text-left w-2/ p-8 mr-96 bg-[#DFE0DC] text-slate-600 rounded-lg">
-                After Vishal Garg’s first attempt to purchase his own dream
-                home, he quickly realized that the homebuying process is
-                unnecessarily broken. This inspired him to found a
-                technology-first company led by engineers and data experts with
-                the mission of digitizing and automating home finance to make it
-                cheaper, easier, and faster for all.
-              </div>
-            </div>
-            <div></div>
-
-            <div className="flex items-center w-full">
-              <div className="w-1/2 text-right pr-8">
-                <div className="p-4 bg-gray-100 rounded-lg shadow-lg">
-                  Content for 2014
-                </div>
-              </div>
-              <div className="w-1/12 text-center">
-                <div className="p-2 bg-green-500 text-white rounded-full">
-                  2014
-                </div>
-              </div>
-            </div>
-
-            {/* 2015 */}
-            <div className="flex items-center w-full">
-              <div className="w-1/12 text-center">
-                <div className="p-2 bg-green-500 text-white rounded-full">
-                  2015
-                </div>
-              </div>
-              <div className="w-1/2 text-left pl-8">
-                <div className="p-4 bg-gray-100 rounded-lg shadow-lg">
-                  Content for 2015
-                </div>
-              </div>
-            </div>
-
-            {/* 2016 */}
-            <div className="flex items-center w-full">
-              <div className="w-1/2 text-right pr-8">
-                <div className="p-4 bg-gray-100 rounded-lg shadow-lg">
-                  Content for 2016
-                </div>
-              </div>
-              <div className="w-1/12 text-center">
-                <div className="p-2 bg-green-500 text-white rounded-full">
-                  2016
-                </div>
-              </div>
-            </div>
-
-            {/* 2017 */}
-            <div className="flex items-center w-full">
-              <div className="w-1/12 text-center">
-                <div className="p-2 bg-green-500 text-white rounded-full">
-                  2017
-                </div>
-              </div>
-              <div className="w-1/2 text-left pl-8">
-                <div className="p-4 bg-gray-100 rounded-lg shadow-lg">
-                  Content for 2017
-                </div>
-              </div>
-            </div>
-
-            {/* Repeat for other years up to 2023 */}
           </div>
         </div>
       </div>
